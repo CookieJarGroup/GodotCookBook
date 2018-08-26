b4236bcf09ac47446585f2840fdbaf249b33d643
Andrés Gutiérrez <andres.gutierrez.arcia@gmail.com>
2018-08-26 11:43:55 -0600

    Agregado de imagenes, y escenas

--- CookiesJar_Game/Scene/menu/menu.gd
+++ CookiesJar_Game/Scene/menu/menu.gd
@@ -11,3 +11,23 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_playSound_pressed():
+	$welcome.play()
+
+
+func _on_welcome_finished():
+	print ("fff")
+
+
+func _on_audio_finished():
+	print ("fff")
+
+# validar mediante alguna estructura de datos*
+func _on_Button_pressed():
+	$Popup.popup()
+
+
+func _on_siguente_pressed():
+	get_tree().change_scene("res://Scene/main.tscn")
